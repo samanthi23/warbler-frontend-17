08b30ca2a524be3989d66a0790a8a2aa6bbb657d
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2021-04-26 13:58:32 +0000

    mapStateToProps

--- warbler-client/src/containers/Main.js
+++ warbler-client/src/containers/Main.js
@@ -1,24 +1,37 @@
 import React from "react";
+// current route and redirect, import props
 import { Switch, Route, withRouter, Redirect } from "react-router-dom";
+
+// make sure to connect to the redux store as well
+// make sure map state to props
 import { connect } from "react-redux";
 // inside of components create a new file named Homepage.js  
 import Homepage from "../components/Homepage";
 
+// create a component called Main
+// run a function that returns the component
 const Main = props => {
   const { authUser } = props;
   return (
     <div className="container">
       <Switch>
+      { /* multiple routes and 404 logic */}
+      { /* pass props to Homepage so that the Homepage can route as well */}
+      {/* renders a function that renders the Homepage Component*/}
         <Route exact path="/" render={props => <Homepage {...props} />} />
       </Switch>
     </div>
   );
 };
 
+// accept state and return an Object
+// make sure map state to props
 function mapStateToProps(state) {
   return {
     currentUser: state.currentUser
   };
 }
 
+// make sure to connect to the redux store as well
+// get props from the router to our component
 export default withRouter(connect(mapStateToProps)(Main));
